ce5e5065096205d7855e92f15e225b6a0ea76c6a
Ron Sims II <ronsims2@gmail.com>
2019-06-24 22:57:22 -0700

    Added friends to hero.

--- csvs_to_json.py
+++ csvs_to_json.py
@@ -1,11 +1,29 @@
-def create_heroes(headers, h):
+def create_heroes(headers, h, f):
 	hero = {}
 	for i,hdr in enumerate(headers):
 		#Strip white space
-		hero[hdr.strip()] = h[i].strip();
-		
+		hero[hdr.strip()] = h[i].strip(); 
+	
+	hero['friends'] = f[hero['id']]
 	print(hero)
 	return hero
+	
+def preprocess_friends():
+	friend_map = {}
+	with open('heroes_friends.csv') as friends:
+		for f in friends:
+			f_vals = f.strip().split(',')
+			if friend_map.get(f_vals[0]):
+				friend_map[f_vals[0]].append(f_vals[1])
+			else:
+				friend_map[f_vals[0]] = [f_vals[1]]
+		
+	return friend_map
+	
+	
+friend_ref = preprocess_friends()
+
+print(friend_ref)
 	
 with open('heroes.csv') as heroes:
 	headers = None
@@ -13,4 +31,4 @@ with open('heroes.csv') as heroes:
 		if i == 0:
 			headers = h.split(',')
 		else:
-			hero_list = create_heroes(headers, h.split(','))
+			hero_list = create_heroes(headers, h.split(','), friend_ref)

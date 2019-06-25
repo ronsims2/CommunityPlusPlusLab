1a03e3bc29e4a05c1395f4d00f427f7d3564892a
Ron Sims II <ronsims2@gmail.com>
2019-06-25 10:05:50 -0700

    Added code to add powers t o json.

--- csvs_to_json.py
+++ csvs_to_json.py
@@ -1,29 +1,32 @@
-def create_heroes(headers, h, f):
+def create_heroes(headers, h, f, p):
 	hero = {}
 	for i,hdr in enumerate(headers):
 		#Strip white space
 		hero[hdr.strip()] = h[i].strip(); 
 	
 	hero['friends'] = f[hero['id']]
+	hero['powers'] = p[hero['id']]
 	print(hero)
 	return hero
 	
-def preprocess_friends():
-	friend_map = {}
-	with open('heroes_friends.csv') as friends:
-		for f in friends:
-			f_vals = f.strip().split(',')
-			if friend_map.get(f_vals[0]):
-				friend_map[f_vals[0]].append(f_vals[1])
+def map_key_pairs_to_list(filename):
+	item_map = {}
+	with open(filename) as items:
+		for item in items:
+			it = item.strip().split(',')
+			if item_map.get(it[0]):
+				item_map[it[0]].append(it[1])
 			else:
-				friend_map[f_vals[0]] = [f_vals[1]]
+				item_map[it[0]] = [it[1]]
 		
-	return friend_map
+	return item_map
 	
 	
-friend_ref = preprocess_friends()
+friend_ref = map_key_pairs_to_list('heroes_friends.csv')
+power_ref = map_key_pairs_to_list('heroes_powers.csv')
 
-print(friend_ref)
+#print(friend_ref)
+#print(power_ref)
 	
 with open('heroes.csv') as heroes:
 	headers = None
@@ -31,4 +34,4 @@ with open('heroes.csv') as heroes:
 		if i == 0:
 			headers = h.split(',')
 		else:
-			hero_list = create_heroes(headers, h.split(','), friend_ref)
+			hero_list = create_heroes(headers, h.split(','), friend_ref, power_ref)
